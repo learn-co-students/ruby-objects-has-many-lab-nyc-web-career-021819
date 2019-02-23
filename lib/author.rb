0445d264e175a78c874653f49387057fae44372b
dlee16 <diana.lee116@gmail.com>
2019-02-23 13:02:47 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+  attr_accessor :name, :posts, :author
+
+  @@all = []
+  def initialize(name)
+    @name = name
+    @posts = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+end
